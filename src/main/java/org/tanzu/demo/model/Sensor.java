00eadc9bf5f9d0c47a129429a76a61444c7c0666
dektlong <dekel.tankel@gmail.com>
2021-12-23 15:36:40 -0800

    Update Sensor.java

--- src/main/java/org/tanzu/demo/model/Sensor.java
+++ src/main/java/org/tanzu/demo/model/Sensor.java
@@ -33,8 +33,6 @@ public class Sensor {
             case 5: this.mood = "Depressed";
         }
         
-        this.team = String.valueOf(doRandom());
-        this.mood = String.valueOf(doRandom());
     }
 
     public Sensor(String team, String mood) {
