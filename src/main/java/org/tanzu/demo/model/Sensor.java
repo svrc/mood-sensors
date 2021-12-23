e0d073a68b8bea19acaa00df2c37c0b454b1cbcd
dektlong <dekel.tankel@gmail.com>
2021-12-23 15:09:55 -0800

    Update Sensor.java

--- src/main/java/org/tanzu/demo/model/Sensor.java
+++ src/main/java/org/tanzu/demo/model/Sensor.java
@@ -3,6 +3,8 @@ package org.tanzu.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 @Entity
 public class Sensor {
@@ -10,26 +12,44 @@ public class Sensor {
     @Id
     @GeneratedValue
     int id;
-    int temperature;
-    int pressure;
+    String team;
+    String mood;
 
     public Sensor() {
     }
 
-    public Sensor(int temperature, int pressure) {
-        this.temperature = temperature;
-        this.pressure = pressure;
+    public Sensor(String team, String mood) {
+        this.team = team;
+        this.mood = mod;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getTemperature() {
-        return temperature;
+    public String getTeam() {
+        //return temperature;
+        switch(doRandom()) {
+            case 1: return "Devs";
+            case 2: return "AppOps";
+            case 3: return "ITOps";
+            case 4: return "Security";
+            case 5: return "DBAs";
+        }
     }
 
-    public int getPressure() {
-        return pressure;
+    public String getMood() {
+        //return pressure;
+        switch(doRandom()) {
+            case 1: return "Happy";
+            case 2: return "Sad";
+            case 3: return "Angry";
+            case 4: return "Scared";
+            case 5: return "Depressed";
+        }
+    }
+    
+    private int doRandom() {
+        return ThreadLocalRandom.current().nextInt(1, 5);
     }
 }
