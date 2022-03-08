b1a9ce5bc1eb71f5ce89616c29f5b4d19b6042c4
dektlong <dekel.tankel@gmail.com>
2022-03-08 09:56:05 -0800

    Update deploy-rabbit.sh

--- deploy-rabbit.sh
+++ deploy-rabbit.sh
@@ -2,6 +2,6 @@
 
 kapp -y deploy --app rmq-operator --file https://github.com/rabbitmq/cluster-operator/releases/download/v1.9.0/cluster-operator.yml
 
-kubectl apply -f supplychain/templates/rabbitmq-clusterrole.yaml
+kubectl apply -f rabbitmq-clusterrole.yaml
 
-kubectl apply -f workloads/devx-mood/rabbitmq-instance.yaml -n $DEMO_APPS_NS
+kubectl apply -f rabbitmq-instance.yaml -n CHANGE_ME
