d58e13bf6d4c23f33b9519491c5acf4bab95c06f
dektlong <dekel.tankel@gmail.com>
2022-03-10 12:41:06 -0800

    Update and rename deploy-rabbit.sh to prep-cluster.sh

--- prep-cluster.sh
+++ prep-cluster.sh
@@ -2,6 +2,4 @@
 
 kapp -y deploy --app rmq-operator --file https://github.com/rabbitmq/cluster-operator/releases/download/v1.9.0/cluster-operator.yml
 
-kubectl apply -f rabbitmq-clusterrole.yaml
-
-kubectl apply -f rabbitmq-instance.yaml -n CHANGE_ME
+kubectl apply -f supplychain
