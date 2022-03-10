99573dc5e51d669e55fd44a1749793bbc8fab127
dektlong <dekel.tankel@gmail.com>
2022-03-10 12:43:34 -0800

    Delete prep-cluster.sh

--- prep-cluster.sh
+++
@@ -1,5 +0,0 @@
-#use this script if running as a stand alone outside of the DektDevX demo
-
-kapp -y deploy --app rmq-operator --file https://github.com/rabbitmq/cluster-operator/releases/download/v1.9.0/cluster-operator.yml
-
-kubectl apply -f supplychain
