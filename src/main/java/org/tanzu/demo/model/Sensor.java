7c5b203042199d8ac7c829dc0bfc138ea7281ca3
dekt <dekel@vmware.com>
2022-08-04 11:49:28 -0700

    change team to planet

--- src/main/java/org/tanzu/demo/model/Sensor.java
+++ src/main/java/org/tanzu/demo/model/Sensor.java
@@ -12,36 +12,43 @@ public class Sensor {
     @Id
     @GeneratedValue
     int id;
-    String type;
+    String planet;
     String mood;
 
     public Sensor() {
         
-        int rand = ThreadLocalRandom.current().nextInt(1, 4);
+        int rand = ThreadLocalRandom.current().nextInt(1, 6);
         if (rand == 1) {
-            this.type = "Devs";
+            this.planet = "Mars";
         } else if (rand == 2) {
-            this.type = "AppOps";
+            this.planet = "Venus";
         } else if (rand == 3) {
-            this.type = "ITOps";
+            this.planet = "Jupiter";
+        } else if (rand == 4) {
+            this.planet = "Neptune";
+        } else if (rand == 5) {
+            this.planet = "Saturn";
         }
+
         
-        rand = ThreadLocalRandom.current().nextInt(1, 6);
+        rand = ThreadLocalRandom.current().nextInt(1, 7);
         if (rand == 1) {
             this.mood = "Happy";
         } else if (rand == 2) {
             this.mood = "Sad";
         } else if (rand == 3) {
-            this.mood = "Angry";
+            this.mood = "Bored";
         } else if (rand == 4) {
             this.mood = "Scared";
         } else if (rand == 5) { 
-            this.mood = "N/A";
+            this.mood = "Clinically Depressed";
+        } else if (rand == 5) { 
+            this.mood = "Socially Angry";
         }
     }
 
-    public Sensor(String type, String mood) {
-        this.type = type;
+    public Sensor(String planet, String mood) {
+        this.planet = planet;
         this.mood = mood;
     }
 
@@ -49,8 +56,8 @@ public class Sensor {
         return id*10;
     }
 
-    public String getType() {
-        return type;
+    public String getPlanet() {
+        return planet;
     }
 
     public String getMood() {
