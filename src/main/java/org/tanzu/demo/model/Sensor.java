02e840c717b8343c5e2ba55a3ed63bad30841b7c
dekel <dekel@vmware.com>
2023-01-04 12:57:42 -0800

    modify to baseline

--- src/main/java/org/tanzu/demo/model/Sensor.java
+++ src/main/java/org/tanzu/demo/model/Sensor.java
@@ -14,19 +14,19 @@ public class Sensor {
     int id;
     String role;
     String mood;
-    String legacy;
+    String baseline;
 
     public Sensor() {
         
         this.role = generateRole();
         this.mood = generateMood();
-        this.legacy = generateLegacy();
+        this.baseline = generateBaseline();
     }
 
-    public Sensor(String role, String mood, String legacy) {
+    public Sensor(String role, String mood, String baseline) {
         this.role = role;
         this.mood = mood;
-        this.legacy = legacy;
+        this.baseline = baseline;
     }
 
     public int getId() {
@@ -41,8 +41,8 @@ public class Sensor {
         return mood;
     }
 
-    public String getLEgacy() {
-        return legacy;
+    public String getBaseline() {
+        return baseline;
     }
     
     private String generateRole() {
@@ -77,17 +77,19 @@ public class Sensor {
         }
     }
 
-    private String generateLegacy() {
+    private String generateBaseline() {
 
-        int rand = ThreadLocalRandom.current().nextInt(1, 5);
+        int rand = ThreadLocalRandom.current().nextInt(1, 11);
+
+        if (rand < 8) {
+                return "";
+        }
         switch(rand) {
-            case 1:
-                return "none";
-            case 2:
+            case 8:
                 return "social issues";
-            case 3:
+            case 9:
                 return "medical condition";
-            case 4:
+            case 10:
                 return "social issues, medical condition";
             default:
                 return "error!!";
